90f8de5d510d07b3b8d0e6454bed4a0c1c81b00f
Francis <1833918721@qq.com>
2020-05-01 14:37:21 +0800

    inline

--- Ham/Classes/iOS/HMProtocol.m
+++ Ham/Classes/iOS/HMProtocol.m
@@ -8,15 +8,15 @@
 #import <Foundation/Foundation.h>
 #import "HMProtocol.h"
 #import <Ham/Ham.h>
-static inline UIViewController* HMGetController(NSString * name,NSDictionary * param,handleControllerCallback callback) {
+inline UIViewController* HMGetController(NSString * name,NSDictionary * param,handleControllerCallback callback) {
     return [InstantProtocol(HMControllerManager) dequeueViewController:name param:param handle:callback];
 }
 
 
-static inline  HMShowRoute(NSString * name,NSDictionary  * _Nullable  param,handleControllerCallback _Nullable callback){
+inline BOOL HMShowRoute(NSString * name,NSDictionary  * _Nullable  param,handleControllerCallback _Nullable callback){
     return [InstantProtocol(HMRouterController) showRoute:name withParam:param callback:callback];
 }
 
-static inline UIViewController* HMGetControllerWithContext(NSString * name,NSDictionary  * _Nullable  param,id<HMControllerCallback> context){
+inline UIViewController* HMGetControllerWithContext(NSString * name,NSDictionary  * _Nullable  param,id<HMControllerCallback> context){
     return [InstantProtocol(HMControllerManager) dequeueViewController:name param:param context:context];
 }
